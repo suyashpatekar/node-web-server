88ac57e5128c4e745f473b998eb318661c1cfa55
suyash.patekar <suyash.patekar@zensar.com>
2020-01-28 16:43:53 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
 })
 
 const getWeather = (location) => {
-    const url = "http://localhost:3000/weather?address="+location
+    const url = "/weather?address="+location
     fetch(url).then((response) => {
         response.json().then((data) => {          
             if(data.error){
